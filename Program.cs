9ff35ed4a668a63862f1b4a1fa1189c94af16b11
Kissdani05 <70641401+Kissdani05@users.noreply.github.com>
2023-05-24 10:24:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Operatorok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> kifejezesek = File.ReadAllLines("Datasource\\kifejezesek.txt").ToList();
+
+            // 2. feladat
+            Console.WriteLine($"2. feladat: Az állomány {kifejezesek.Count} kifejezést tartalmaz.");
+
+            // 3. feladat
+
+            Console.WriteLine($"3. feladat: A maradékos osztást tartalmazó kifejezések száma: {kifejezesek.Count(x => x.Split(" ")[1] == "mod")}");
+
+            // 4. feladat
+
+            Console.WriteLine($"4. feladat: {(kifejezesek.Any(x => int.Parse(x.Split(" ")[0]) % 10 == 0 && int.Parse(x.Split(" ")[2]) % 10 == 0) ? "Van" : "Nincs")} olyan kifejezés, amelyben mindkét operandus maradék nélkül osztható tízzel.");
+
+            
+        }
+    }
+}
